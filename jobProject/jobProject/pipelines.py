a5c296feb0639a3a9f1d82736af5b896e381cb31
Mohammad Mirzaei <mirzaei_1378@yahoo.com>
2024-06-12 01:59:59 -0700

    job entries are saved in PostgreSQL table columns

--- jobProject/jobProject/pipelines.py
+++ jobProject/jobProject/pipelines.py
@@ -1,46 +1,57 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 import psycopg2
-
-# useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-        
 
 class SaveToPostgresql:
 
     def __init__(self):
         self.connection = psycopg2.connect(
-            host = "localhost",
-            user = "mohammadmirzaei",
-            database = "jobProject",
-            port ="5432",
+            host="localhost",
+            user="mohammadmirzaei",
+            password="your_password",  # Replace with your actual password
+            database="jobProject",
+            port="5432",
         )
         self.cursor = self.connection.cursor()
-        self.create_raw_table()
-
+        self.create_raw_table()  # Create the table if it doesn't exist
 
     def create_raw_table(self):
         self.cursor.execute("""
             CREATE TABLE IF NOT EXISTS raw_table (
-                jobid SERIAL PRIMARY KEY,
-                data TEXT
-                );
+                jobid SERIAL PRIMARY KEY
+            );
         """)
-        self.connection.commit() 
-    
-    def process_item(self, item, spider):
-        self.cursor.execute("""
-            INSERT INTO raw_table (data)
-            VALUES (%s)
-            RETURNING jobid;
-        """, (str(item['data']),) )
         self.connection.commit()
+
+    def update_table_columns(self, column): # Here if the column does not exist in database, add it to the table
+        self.cursor.execute("""
+            DO $$
+            BEGIN
+                IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'raw_table' AND column_name = %s) THEN
+                    EXECUTE 'ALTER TABLE raw_table ADD COLUMN ' || quote_ident(%s) || ' TEXT';
+                END IF;
+            END $$;
+        """, (column, column))
+        self.connection.commit()
+
+    def process_item(self, item, spider):
+        data = item['data']
+        item_keys = data.keys()
+        # for each column in job item , if it does not exist in database it will update the database table
+        for column in item_keys:
+            self.update_table_columns(column)
+
+        # Construct the sql insert statement 
+        placeholders = ', '.join(['%s'] * len(item_keys))
+        columns = ', '.join(item_keys)
+        sql_query = f"INSERT INTO raw_table ({columns}) VALUES ({placeholders}) RETURNING jobid;"
+
+        values = [str(data[key]) for key in item_keys]  # Ensure values are converted to string for insertion
+        self.cursor.execute(sql_query, values)
+        self.connection.commit()
+
         return item
 
-
     def __del__(self):
-        """ close the connection to database on destrcutor of object"""
+        """ Close the connection to database in the destructor """
+        self.cursor.close()
         self.connection.close()
-
